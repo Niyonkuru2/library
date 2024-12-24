560972f7ccade1b2f5944fb81d403bc76bebd4de
Niyonkuru2 <150118511+Niyonkuru2@users.noreply.github.com>
2024-12-24 02:38:38 +0200

    Update ResrvedAndIsued.jsx

--- client/src/pages/ResrvedAndIsued.jsx
+++ client/src/pages/ResrvedAndIsued.jsx
@@ -23,7 +23,7 @@ const YourBooks = () => {
   };
 
   return (
-    <div className="container mx-auto p-6">
+    <div className="container mx-auto p-6 min-h-screen">
       <h1 className="text-3xl font-bold text-center mb-6">Your Books</h1>
 
       <div className="mb-4">
